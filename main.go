9a107804fbd391c3999f192412a8a44f3f9e7135
unknown <yue@yanchangyue.mango.com>
2020-06-23 09:57:25 +0800

    项目修改

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"pm2go/lib"
+	"os"
+)
+
+func main() {
+	configPath := flag.String("c", "", "config file path")
+	flag.Parse()
+
+	if *configPath != "" {
+		if _, err := os.Stat(*configPath); err != nil {
+			fmt.Printf("Can't find config file `%s`\n", *configPath)
+			os.Exit(1)
+		}else {
+			os.Setenv("RUNNER_CONFIG_PATH", *configPath)
+		}
+	}
+
+	lib.Start()
+}
